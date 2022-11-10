2f9819db24c39e66c357925bdb2faf5bb9a65c98
Migara-Pramod <migarapramodm@gmail.com>
2022-11-10 16:05:47 +0530

    Temporary remove the test folder

--- test/conformance/purge.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-cd "$( dirname "${BASH_SOURCE[0]}" )/../.."
-
-docker-compose -f quickstart.yml -f quickstart-postgres.yml -f test/conformance/docker-compose.yml down -v
--- test/conformance/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-
-go test -tags conformity -test.timeout 60m -failfast "$@" .
